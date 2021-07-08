cd4b38535688db057ca11266ac4631c00a1bdc87
andreagorno <andreagorno98@gmail.com>
2021-07-08 15:09:38 +0200

    esercizio svolto

---
+++ index.php
@@ -0,0 +1,127 @@
+<?php 
+    $description1 = "Hercules, nella leggenda un semidio figlio di Zeus ma in realtà un orfano ateniese, mortale ma dotato di una forza sovrumana, reduce dalle dodici fatiche affidategli dal re di Micene Euristeo per espiare il massacro della sua stessa famiglia, guida un gruppo di fedeli ed esperti mercenari: l'ironico veggente argivo Anfiarao, il ladro spartano Autolico, l'amazzone scitica Atlanta e il selvaggio e taciturno tebano Tideo, oltre al proprio nipote Iolao, promoter dell'eroe e cantore delle sue gesta. Hercules e i suoi compagni vengono assoldati dal re Cotys per por fine a una sanguinosa guerra civile nelle terre di Tracia. Il nemico che l'anziano monarca indica all'eroe è Reso, a capo di una potente e numerosa armata che saccheggia e devasta il suo regno e che si dice formata dalle mitologiche figure dei centauri.
+
+
+    L'esercito tracio, sotto l'esperta guida del gruppo di mercenari, diventa ben presto un'armata ben organizzata e bene armata e, nella successiva battaglia contro l'armata di Reso, esce vincitrice. I capi dell'esercito nemico vengono portati in catene in Tracia e qui il re catturato fa sorgere il dubbio ad Hercules sulla parte che ha deciso di difendere. Il mercenario scopre presto che Cotys è in realtà un vile usurpatore assetato di potere che non ha esitato ad avvelenare il marito di sua figlia pur di conservare il trono e si è alleato con Euristeo, il quale, una volta che Hercules viene catturato insieme ai suoi compagni d'arme, gli rivela di essere il vero mandante dello sterminio della sua famiglia, eseguito da tre enormi e famelici lupi.
+
+    Hercules, ormai prigioniero di Cotys e incatenato nelle segrete del suo palazzo, si libera dei suoi demoni interiori e, dopo avere eliminato i tre lupi che uccisero la sua famiglia, libera i suoi compagni e, con il loro aiuto, uccide prima Euristeo, poi Cotys e il suo generale. Nella battaglia finale, grazie anche al sacrificio del fido Tideo, Hercules riesce a salvare sia Ergenia che Areo, venendo poi riconosciuto come unico e vero leader dall'esercito trace.";
+
+    $description2 = "Nel I secolo a.C., la Repubblica romana è scivolata nella corruzione, e i lavori umili sono svolti da eserciti di schiavi. Uno di questi, un orgoglioso e dotato trace di nome Spartaco, è così poco collaborativo nella sua posizione in una fossa mineraria che viene condannato a morte per fame. Per caso, viene mostrato allo scaltro uomo d'affari romano Lentulo Batiato, che – colpito dalla sua ferocia – acquista Spartaco per la sua scuola di gladiatori, dove ordina all'allenatore Marcello di non esagerare con lui perché pensa che 'abbia delle qualità'. Tra un maltrattamento e l'altro, Spartacus stringe un rapporto silenzioso con una donna di servizio di nome Varinia, che rifiuta di violentare quando viene mandata a 'intrattenerlo' nella sua cella. Spartaco e Varinia sono successivamente costretti a sopportare numerose umiliazioni per aver sfidato le condizioni della servitù.
+    Batiato riceve una visita dal ricco senatore romano Marco Licinio Crasso, che mira a diventare dittatore della stagnante repubblica. Crasso acquista Varinia per capriccio, e per il divertimento dei suoi compagni fa combattere a morte due coppie di gladiatori, tra i quali vi è Spartaco. Quando quest'ultimo viene disarmato, il suo avversario, un africano di nome Draba, risparmia la sua vita in un'esplosione di sfida e attacca il pubblico romano, ma viene ucciso da Crasso. Il giorno successivo, con l'atmosfera ancora tesa su questo episodio, Batiato porta Varinia a casa di Crasso a Roma. Spartaco uccide Marcello, che lo stava provocando a tal proposito, e la loro lotta si intensifica in una rivolta. I gladiatori sopraffanno le loro guardie e scappano nella campagna italiana.
+    
+    Spartaco viene eletto capo dei fuggitivi e decide di condurli fuori dall'Italia e tornare alle loro case. Saccheggiano le tenute di campagna romane lungo il cammino, raccogliendo abbastanza soldi per acquistare il trasporto marittimo dai nemici di Roma, i pirati della Cilicia. Innumerevoli altri schiavi si uniscono al gruppo, rendendolo grande come un esercito. Uno dei nuovi arrivati è Varinia, che era fuggita prima di essere consegnata a Crasso. Un altro è uno schiavo intrattenitore di nome Antonino, anch'egli fuggito dal servizio di Crasso. In privato, Spartaco si sente mentalmente inadeguato a causa della sua mancanza di istruzione durante gli anni di servitù. Tuttavia, si dimostra un leader eccellente e organizza i suoi diversi seguaci in una comunità forte e autosufficiente. Inoltre concepisce un figlio con Varinia, ora sua moglie informale, e arriva anche a considerare lo spiritoso Antonino come una sorta di figlio.
+
+    Il Senato romano è sempre più allarmato mentre Spartaco sconfigge i molteplici eserciti che gli manda contro. L'avversario populista di Crasso, Gracco, sa che il suo rivale cercherà di usare la crisi come giustificazione per prendere il controllo dell'esercito romano. Per cercare di impedirlo, Gracco canalizza quanta più potenza militare possibile nelle mani del proprio protetto, un giovane senatore di nome Giulio Cesare. Sebbene Cesare manchi del disprezzo di Crasso per le classi inferiori di Roma, scambia la rigida visione dell'uomo per nobiltà. Quindi, quando Gracco rivela di aver corrotto i ciliciani per far uscire Spartaco dall'Italia e liberare Roma dall'esercito di schiavi, Cesare considera tali tattiche come inferiori a lui e passa dalla parte di Crasso.
+
+    Crasso paga i pirati perché abbandonino Spartaco, e fa sì che l'esercito romano costringa segretamente i ribelli ad allontanarsi dalla costa, avvicinandoli a Roma. Temendo che Spartaco intenda saccheggiare la città, il Senato conferisce a Crasso il potere assoluto. Circondato dai romani, Spartaco convince i suoi uomini a morire combattendo, poiché solo ribellandosi e dimostrandosi umani hanno sferrato un colpo contro la schiavitù. Nella battaglia che ne segue, dopo aver inizialmente rotto le righe delle legioni di Crasso, l'esercito di schiavi finisce intrappolato tra Crasso e altre due forze che avanzano da dietro, e la maggior parte di loro viene massacrata. In seguito, i romani provano a localizzare il leader ribelle per una punizione esemplare offrendo il perdono (e il ritorno alla schiavitù) se gli uomini identificheranno Spartaco, vivo o morto. Ogni uomo sopravvissuto risponde gridando 'Io sono Spartaco!'. Di conseguenza, Crasso li condanna tutti a morte per crocifissione lungo la via Appia tra Roma e Capua, dove era iniziata la rivolta.
+
+    Crasso trova Varinia e il figlio neonato di Spartaco e li fa prigionieri. È turbato dall'idea che Spartaco possa ottenere più amore e lealtà di quanto non riesca lui, e spera di compensare rendendo Varinia devota a lui come lo era al suo ex marito. Quando lei lo rifiuta, Crasso cerca furiosamente Spartaco (che riconosce avendolo visto nella scuola di Batiato) e lo costringe a combattere contro Antonino fino alla morte. Il sopravvissuto verrà crocifisso, insieme a tutti gli altri uomini catturati dopo la grande battaglia. Spartaco uccide Antonino per risparmiargli questo terribile destino. L'incidente lascia Crasso preoccupato per il potenziale di Spartaco di vivere nella leggenda come un martire. Inoltre è preoccupato per Cesare, che secondo lui un giorno lo eclisserà.
+
+    Gracco, avendo visto Roma cadere nella tirannia, si suicida, ma non prima di aver fatto in modo che Batiato salvasse la famiglia di Spartaco da Crasso e li portasse via in libertà. Uscendo da Roma, il gruppo passa sotto la croce di Spartaco. Varinia riesce a confortarlo nei suoi ultimi istanti di vita mostrandogli il suo figlioletto, che crescerà libero e sapendo chi era suo padre.";
+
+    $description3 = "Circa nel 1200 a.C., Agamennone è re di tutte le città-stato della Grecia. Solo la potente città di Troia, conosciuta per le sue potenti mura difensive erette dagli dei, è rimasta inviolata.
+
+    Il film incomincia con una lotta fra l'esercito di Micene guidato da Agamennone e l'esercito della Tessaglia guidato da Triopa, schierati l'uno contro l'altro in una piana. Per risparmiare la vita dei propri uomini, i due re mettono in campo rispettivamente i loro migliori guerrieri. Contro l'enorme campione della Tessaglia Boagrius (un vero gigante dal volto segnato da cicatrici), Agamennone schiera Achille, sovrano di Ftia e capo dei Mirmidoni, che vince con grande facilità. Palese è comunque l'insofferenza che Achille manifesta verso Agamennone, a causa della sua posizione di subalterno al re greco, disobbedendogli spesso ed eseguendo di malavoglia i suoi ordini. Intanto una delegazione di Troiani, guidata dai due figli del re Priamo, Ettore e Paride, si trova a Sparta per stipulare una ferrea promessa di pace tra le due superpotenze con il re Menelao, fratello di Agamennone. Durante la loro permanenza, nasce l'appassionata relazione tra il giovane principe Paride ed Elena, moglie di Menelao.
+    
+    Nel corso del viaggio di ritorno, Paride rivela al fratello di aver portato con sé Elena, incontrando la sua ira e disapprovazione: Agamennone e Menelao infatti utilizzeranno la scomparsa di Elena come pretesto per muovere guerra a Troia: erano anni che Agamennone, bramoso di potere, desiderava quest'occasione per prendere il potere sulla città e avere così il controllo su tutto il mar Egeo. Ciò nonostante, la giovane regina spartana viene accolta con grande affetto nella città, le cui mura si presume resistano a qualunque assalto perché edificate dagli dei Poseidone e Apollo. Come previsto, Agamennone comincia a radunare l'esercito contro Troia, ma Achille si rifiuta di scendere in guerra. Il re manda così Ulisse, re di Itaca e il più scaltro dei greci, a reclutare Achille a Ftia. Questi è riluttante a sottomettersi al comando di Agamennone, stante i loro cattivi rapporti, e Ulisse prova a irretire il cugino di Achille, Patroclo, narrandogli delle meraviglie e delle ricchezze della guerra, sapendo che Achille non lo avrebbe mai lasciato andare da solo. Ancora indeciso, Achille viene avvertito dalla madre, la dea Teti: lui può scegliere se vivere a lungo restando in patria vivendo una vita felice, ma venendo presto dimenticato dopo la morte, o se andare a cercare la gloria a Troia e morirvi dopo averla trovata. Stimolato dalla possibilità di avere gloria eterna, Achille sceglie la seconda opzione e con 50 Mirmidoni, tra cui Eudoro, il suo alleato più fedele, si imbarca alla volta di Troia, assieme a una flotta di oltre mille navi greche.
+    
+    Achille e i suoi guerrieri svolgeranno un ruolo cruciale nell'assalto a Troia, sebbene contrastati dall'esercito dei troiani comandato da Ettore, il più valoroso del suo popolo.
+    
+    Senza attendere il resto della flotta, Achille e i suoi Mirmidoni sbarcano sulla spiaggia di Troia e dopo una dura lotta hanno la meglio sui nemici, raggiungendo il tempio di Apollo, dove uccidono tutti i sacerdoti: per manifestare il suo supremo sprezzo nei confronti del fato, alla domanda di Eudoro, che gli chiede se la loro azione non sia stata blasfema, il Pelide (così è soprannominato Achille dato che è figlio di Peleo) decapita con la spada la statua d'oro di Apollo. All'avvicinarsi di un gruppo di cavalieri guidati da Ettore, il principale difensore di Troia, Achille, per dimostrare la sua potenza e capacità sovrumana, scaglia da grandissima distanza una picca contro Tettone, compagno dell'eroe troiano, riuscendo grazie alla tecnica, potenza e mira incredibile a centrarlo sul collo e buttarlo giù da cavallo. Ettore, quasi incredulo di quanto Achille sia riuscito a fare, continua ad avanzare cercando di colpire il guerriero greco con la sua picca, ma il Piè Veloce (altro epiteto di Achille) riesce a schivarla spostandosi rapidamente, mostrando un sorriso quasi beffardo nei confronti dell'eroe troiano. Ettore, insieme con i suoi guerrieri, insegue l'acheo all'interno del tempio. Al termine di una furiosa battaglia in cui Achille vede Ettore battersi con grande valore, l'eroe greco gli intima di andarsene in quanto è troppo presto perché muoiano i principi; dalla faccia di Achille s'intravede l'ammirazione che ha per Ettore in quanto fortissimo guerriero e uomo buono. Ettore se ne va, mentre Achille viene raggiunto dagli altri greci, tra cui Aiace Telamonio, Menelao, Ulisse e Agamennone, felici della vittoria che ha permesso la facile conquista in un sol giorno della spiaggia troiana.
+    
+    Al tempio di Apollo, Achille ha fatto schiava Briseide, nota sacerdotessa di Apollo e cugina dei principi Ettore e Paride. Costei si ribella ad Achille, che però si comporta in maniera molto gentile nei suoi riguardi in quanto è fortemente attratto dal coraggio e dal carattere ribelle della giovane donna: poi, tra i due comincia a nascere una reciproca stima che diventa ben presto amore. Venutone a conoscenza, Agamennone reclama Briseide per sé ed è solo grazie all'intervento della stessa schiava che Achille non uccide il re: furioso, il guerriero si rifiuta di combattere finché lei non gli verrà restituita, ma Agamennone non cede.";
+?>
+<?php
+    class Movie {
+        // POPRIETA'
+        public $title;
+        public $or_language;
+        public $year;
+        public $description;
+
+        // COSTRUTTORE
+        function __construct($title, $or_language, $year, $description) {
+            $this->title = $title;
+            $this->or_language = $or_language;
+            $this->year = $year;
+            $this->description = $description;
+        }
+
+        // METODI
+        public function getExcerpt($charsNumber = 200) {
+            return substr($this->description, 0, $charsNumber) . "...";
+        }
+    }
+
+    
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <title>php-oop-1</title>
+    </head>
+    <body>
+        <?php 
+            $movie1 = new Movie ("Hercules", "en", "2013", $description1);
+            
+            $movie2 = new Movie ("Spartacus", "en", "2008", $description2);
+
+            $movie3 = new Movie ("Achille", "it", "2005", $description3);
+
+            // var_dump($movie1);
+        ?>
+        
+        <main>
+            <div>
+                <h2>
+                    Titolo Originale: <?= $movie1->title; ?>
+                </h2>
+                <h3>
+                    Lingua Originale: <?= $movie1->or_language; ?>
+                </h3>
+                <h4>
+                    Anno di rilascio: <?= $movie1->year; ?>
+                </h4>
+                <p>
+                    Trama: <small><?= $movie1->getExcerpt(300); ?></small>
+                </p>
+            </div>
+
+            <div>
+                <h2>
+                    Titolo Originale: <?= $movie2->title; ?>
+                </h2>
+                <h3>
+                    Lingua Originale: <?= $movie2->or_language; ?>
+                </h3>
+                <h4>
+                    Anno di rilascio: <?= $movie2->year; ?>
+                </h4>
+                <p>
+                    Trama: <small><?= $movie2->getExcerpt(300); ?></small>
+                </p>
+            </div>
+            <div>
+                <h2>
+                    Titolo Originale: <?= $movie3->title; ?>
+                </h2>
+                <h3>
+                    Lingua Originale: <?= $movie3->or_language; ?>
+                </h3>
+                <h4>
+                    Anno di rilascio: <?= $movie3->year; ?>
+                </h4>
+                <p>
+                    Trama: <small><?= $movie3->getExcerpt(300); ?></small>
+                </p>
+            </div>
+        </main>
+    </body>
+</html>
